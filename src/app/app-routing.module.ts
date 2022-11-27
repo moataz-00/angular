54832a89c69d6f31b0e1c2039b9c7f186790bb4b
moataz-00 <tahamoataz5@gmail.com>
2022-11-27 21:01:11 +0200

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"home", component:HomeComponent},
+  {path:"**", component:ErrorComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  
+exports: [RouterModule]
+})
+export class AppRoutingModule { }
